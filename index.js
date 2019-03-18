dd01d2ef116581fb3cabd1f68069ea5a68cb54dc
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-03-18 15:20:45 +0100

    Added DELETED and Put Methods. MVP Done

--- index.js
+++ index.js
@@ -28,4 +28,40 @@ server.get('/api/users/:id', (req, res) => {
   .catch(err => res.status(404).json({ error: "The user with the specified ID does not exist."}))
 })
 
+server.delete('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+  db.remove(req.params.id)
+    .then(data => {
+      if (!data) {
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+      }
+      else {
+        res.status(200).json({ message: `Deleted user with ID ${id}`})
+      }
+    })
+    .catch(err => res.status(500).json({ error: "The user could not be removed" }))
+})
+
+server.put('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+  const user = req.body;
+
+  if (!user.name || !user.bio) {
+    res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+  }
+  db.update(id, user)
+    .then(data => {
+      if (!data) {
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+      }
+      else {
+        res.status(200).json({message: "The user was updated", user: {...user, id}})
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The user information could not be modified." })
+    })
+})
+
+
 server.listen(4000, () => console.log(`Running on port 4000`))
