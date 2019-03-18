edb82c7216c9ad87cb78a658bac72cfdb43191ff
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-03-18 13:35:48 +0100

    Initial Commit. Added Express

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db')
+const server = express();
+
+
+
+server.listen(4000, () => console.log(`Running on port 4000`))
